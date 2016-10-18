1bc0596b2c42f67f5533031ec78bb4f839197b61
Ahmad Bawaneh <akabme@gmail.com>
2016-10-18 09:47:54 +0300

    refactoring

--- Copier/copier-annotation-processor/src/main/java/com/progressoft/annotation/processor/CopierAnnotationProcessor.java
+++ Copier/copier-annotation-processor/src/main/java/com/progressoft/annotation/processor/CopierAnnotationProcessor.java
@@ -25,22 +25,7 @@ import java.util.Set;
 @SupportedSourceVersion(value = SourceVersion.RELEASE_8)
 public class CopierAnnotationProcessor extends AbstractProcessor {
 
-    private final FieldsCopyStatementGenerator simpleFieldAccessorGenerator = new AccessorSingleFieldCopyGenerator();
-    private final FieldsCopyStatementGenerator simplePublicFieldGenerator = new SimplePublicFieldCopyGenerator();
 
-    private final Map<String, FieldsCopyStatementGenerator> collectionAccessorGenerators=new HashMap<String, FieldsCopyStatementGenerator>(){{
-        put(CollectionCopy.ARRAY_LIST, new ArrayListAccessorCopier());
-        put(CollectionCopy.HASH_SET, new HashSetAccessorCopier());
-        put(CollectionCopy.LINKED_LIST, new LinkedListAccessorCopier());
-        put(CollectionCopy.HASH_MAP, new HashMapAccessorCopier());
-    }};
-
-    private final Map<String, FieldsCopyStatementGenerator> collectionPublicFieldsGenerators=new HashMap<String, FieldsCopyStatementGenerator>(){{
-        put(CollectionCopy.ARRAY_LIST, new ArrayListPublicFieldGenerator());
-        put(CollectionCopy.HASH_SET, new HashSetPublicFieldGenerator());
-        put(CollectionCopy.LINKED_LIST, new LinkedListPublicFieldGenerator());
-        put(CollectionCopy.HASH_MAP, new HashMapPublicFieldGenerator());
-    }};
 
     private Types typesUtil;
     private Elements elementsUtil;
@@ -71,8 +56,6 @@ public class CopierAnnotationProcessor extends AbstractProcessor {
 
         try (Writer sourceWriter = obtainSourceWriter(className, annotatedClassPackage)) {
             String result = writeCopierClass(annotatedClassElement, className, annotatedClassPackage);
-            System.out.println(result);
-            System.out.println("==========================");
             sourceWriter.write(result);
         } catch (IOException e) {
             messager.printMessage(Diagnostic.Kind.ERROR, "could not generate class");
@@ -159,27 +142,10 @@ public class CopierAnnotationProcessor extends AbstractProcessor {
         return annotatedClassElement.getAnnotation(WithCopier.class);
     }
 
-    private FieldsCopyStatementGenerator selectedGenerator(TypeElement annotatedClassElement, Element element) {
-        WithCopier withCopier=annotatedClassElement.getAnnotation(WithCopier.class);
-        switch (withCopier.mode()){
-            case GETTERS_SETTERS:
-                if(Objects.nonNull(element.getAnnotation(CollectionCopy.class))){
-                    return collectionAccessorGenerators.get(element.getAnnotation(CollectionCopy.class).initializer());
-                }
-                return simpleFieldAccessorGenerator;
-            case PUBLIC_MEMEBERS:
-                if(Objects.nonNull(element.getAnnotation(CollectionCopy.class))){
-                    return collectionPublicFieldsGenerators.get(element.getAnnotation(CollectionCopy.class).initializer());
-                }
-                return simplePublicFieldGenerator;
-        }
-        return simpleFieldAccessorGenerator;
-    }
-
     private void generatedCopStatements(TypeElement annotatedClassElement, StringBuilder sb) {
         for (Element element : annotatedClassElement.getEnclosedElements())
             if (element.getKind() == ElementKind.FIELD && notIgnored(element))
-                sb.append(selectedGenerator(annotatedClassElement, element).generate(element));
+                sb.append(FieldCopyStatementFactory.getGenerator(annotatedClassElement, element).generate(element));
     }
 
     private boolean notIgnored(Element element) {
---
+++ Copier/copier-annotation-processor/src/main/java/com/progressoft/annotation/processor/FieldCopyStatementFactory.java
@@ -0,0 +1,44 @@
+package com.progressoft.annotation.processor;
+
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class FieldCopyStatementFactory {
+
+    private static final FieldsCopyStatementGenerator simpleFieldAccessorGenerator = new AccessorSingleFieldCopyGenerator();
+    private static final FieldsCopyStatementGenerator simplePublicFieldGenerator = new SimplePublicFieldCopyGenerator();
+
+    private static final Map<String, FieldsCopyStatementGenerator> collectionAccessorGenerators=new HashMap<String, FieldsCopyStatementGenerator>(){{
+        put(CollectionCopy.ARRAY_LIST, new ArrayListAccessorCopier());
+        put(CollectionCopy.HASH_SET, new HashSetAccessorCopier());
+        put(CollectionCopy.LINKED_LIST, new LinkedListAccessorCopier());
+        put(CollectionCopy.HASH_MAP, new HashMapAccessorCopier());
+    }};
+
+    private static final Map<String, FieldsCopyStatementGenerator> collectionPublicFieldsGenerators=new HashMap<String, FieldsCopyStatementGenerator>(){{
+        put(CollectionCopy.ARRAY_LIST, new ArrayListPublicFieldGenerator());
+        put(CollectionCopy.HASH_SET, new HashSetPublicFieldGenerator());
+        put(CollectionCopy.LINKED_LIST, new LinkedListPublicFieldGenerator());
+        put(CollectionCopy.HASH_MAP, new HashMapPublicFieldGenerator());
+    }};
+
+    public static FieldsCopyStatementGenerator getGenerator(TypeElement annotatedClassElement, Element element) {
+        WithCopier withCopier=annotatedClassElement.getAnnotation(WithCopier.class);
+        switch (withCopier.mode()){
+            case GETTERS_SETTERS:
+                if(Objects.nonNull(element.getAnnotation(CollectionCopy.class))){
+                    return collectionAccessorGenerators.get(element.getAnnotation(CollectionCopy.class).initializer());
+                }
+                return simpleFieldAccessorGenerator;
+            case PUBLIC_MEMEBERS:
+                if(Objects.nonNull(element.getAnnotation(CollectionCopy.class))){
+                    return collectionPublicFieldsGenerators.get(element.getAnnotation(CollectionCopy.class).initializer());
+                }
+                return simplePublicFieldGenerator;
+        }
+        return simpleFieldAccessorGenerator;
+    }
+}
